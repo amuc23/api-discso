f26520164a09affdcddc181992014d81716f6b86
Alonso <amuc23@gmail.com>
2024-10-21 19:29:24 -0300

    cambios de productos2

--- discos.js
+++ discos.js
@@ -4,21 +4,21 @@ const discos = [
     "album": "4444",
     "artista": "Woosung",
     "fecha_estreno": 25/12/2024,
-    "imagen": "https://www.google.com/search?sca_esv=bfb74702882ef790&rlz=1C1CHBF_esCL1074CL1074&cs=1&q=kim+woo-sung+4444&udm=2&fbs=AEQNm0CbCVgAZ5mWEJDg6aoPVcBgWizR0-0aFOH11Sb5tlNhdzTfxpAVBoexMFZnKJBpl_OnTFfcge8advfRBIXE7C_RnVwHxQQjt3s5IDAD2SsyzotijuTOHN3vWoJwcmhPScK0uW5iCpjFSlXv7E-18agrL8kHbtNL5OLwW1zfvexaV-xbAWNsYlua1vIU0WWB8rdOckn0Dk8Zh_wwwALBryZaclBaEg&sa=X&ved=2ahUKEwi-9POlyJ6JAxUkHbkGHYRnIwoQtKgLegQIIxAB&biw=1366&bih=633&dpr=1#vhid=IhC6RVnAVRGVhM&vssid=mosaic"
+    "imagen": "https://cdnx.jumpseller.com/tienda-humo/image/52630125/resize/1000/1000?1725560221"
   },
   {
     "id": 2,
     "album": "Dual",
     "artista": "The Rose",
     "fecha": 30/12/2024,
-    "imagen": "https://www.google.com/search?cs=1&rlz=1C1CHBF_esCL1074CL1074&sca_esv=bfb74702882ef790&q=the+rose+dual&udm=2&fbs=AEQNm0CbCVgAZ5mWEJDg6aoPVcBgWizR0-0aFOH11Sb5tlNhd7Qv31WAq-g3XdD7m281OKwFyA-0X_WEQGTRyn1HvFYUfg4962s3IQpiF9A4nifDyRfLbDMpP9pZXfaPeccn79uTqpC_f5_vUtusyortnlx34wxNRyZavSGi5OeGmKyzoZ5AujJhv9levWsIatIBqNT6hLjNt13NxsS6BJdEkN8yAzO0Dw&sa=X&ved=2ahUKEwiLmqHHyJ6JAxX3HLkGHVzJOo8QtKgLegQIIhAB&biw=1366&bih=633&dpr=1#vhid=HZLpiAPY0DIm8M&vssid=mosaic"
+    "imagen": "https://cdnx.jumpseller.com/cakepopundertaking/image/41135919/DUAL_COVER.png?1697664211"
   },
   {
     "id": 3,
     "album": "SPILL THE FEELS",
     "artista": "SEVENTEEN",
     "fecha": 28/10/2024,
-    "imagen": "https://www.google.com/search?cs=1&rlz=1C1CHBF_esCL1074CL1074&sca_esv=bfb74702882ef790&q=seventeen+spill+the+feels&udm=2&fbs=AEQNm0CbCVgAZ5mWEJDg6aoPVcBgy3z6G8XHXm5Ah4uwilWjRJeyTIczQNFLgz4BekoSgG70mFPAcMtEFe4JLi1nQt_XmejUymD71cdvQ9jZVXr2fVtK1FtnrBHdRwPPC5ShYFKURYNiccuiaVTNh_e3LL-2SHsFlfCR3A4DKm4TVI-72ag_1MrXykndYrrZLgPlOs3MMBiUM8WaWIkcrm9yM_Ngilj8sQ&sa=X&ved=2ahUKEwjjrqvwyJ6JAxU0LbkGHdhJIsMQtKgLegQIExAB&biw=1366&bih=633&dpr=1#vhid=vmuZdifHIzQi7M&vssid=mosaic"
+    "imagen": "https://i.scdn.co/image/ab67616d0000b273cb657e50a77865fe66d700bf"
   }
 ];
 
