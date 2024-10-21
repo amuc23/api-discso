5d015addbf9cbb9f3ee059bcf4984a609396f970
Alonso <amuc23@gmail.com>
2024-10-21 19:22:27 -0300

    cambios de productos

--- discos.js
+++ discos.js
@@ -1,24 +1,24 @@
 const discos = [
   {
     "id": 1,
-    "titulo": "Thriller",
-    "artista": "Michael Jackson",
-    "anio": 1982,
-    "imagen": "https://link-a-imagen-de-thriller.com"
+    "album": "4444",
+    "artista": "Woosung",
+    "fecha_estreno": 25/12/2024,
+    "imagen": "https://www.google.com/search?sca_esv=bfb74702882ef790&rlz=1C1CHBF_esCL1074CL1074&cs=1&q=kim+woo-sung+4444&udm=2&fbs=AEQNm0CbCVgAZ5mWEJDg6aoPVcBgWizR0-0aFOH11Sb5tlNhdzTfxpAVBoexMFZnKJBpl_OnTFfcge8advfRBIXE7C_RnVwHxQQjt3s5IDAD2SsyzotijuTOHN3vWoJwcmhPScK0uW5iCpjFSlXv7E-18agrL8kHbtNL5OLwW1zfvexaV-xbAWNsYlua1vIU0WWB8rdOckn0Dk8Zh_wwwALBryZaclBaEg&sa=X&ved=2ahUKEwi-9POlyJ6JAxUkHbkGHYRnIwoQtKgLegQIIxAB&biw=1366&bih=633&dpr=1#vhid=IhC6RVnAVRGVhM&vssid=mosaic"
   },
   {
     "id": 2,
-    "titulo": "The Dark Side of the Moon",
-    "artista": "Pink Floyd",
-    "anio": 1973,
-    "imagen": "https://link-a-imagen-de-dark-side.com"
+    "album": "Dual",
+    "artista": "The Rose",
+    "fecha": 30/12/2024,
+    "imagen": "https://www.google.com/search?cs=1&rlz=1C1CHBF_esCL1074CL1074&sca_esv=bfb74702882ef790&q=the+rose+dual&udm=2&fbs=AEQNm0CbCVgAZ5mWEJDg6aoPVcBgWizR0-0aFOH11Sb5tlNhd7Qv31WAq-g3XdD7m281OKwFyA-0X_WEQGTRyn1HvFYUfg4962s3IQpiF9A4nifDyRfLbDMpP9pZXfaPeccn79uTqpC_f5_vUtusyortnlx34wxNRyZavSGi5OeGmKyzoZ5AujJhv9levWsIatIBqNT6hLjNt13NxsS6BJdEkN8yAzO0Dw&sa=X&ved=2ahUKEwiLmqHHyJ6JAxX3HLkGHVzJOo8QtKgLegQIIhAB&biw=1366&bih=633&dpr=1#vhid=HZLpiAPY0DIm8M&vssid=mosaic"
   },
   {
     "id": 3,
-    "titulo": "Back in Black",
-    "artista": "AC/DC",
-    "anio": 1980,
-    "imagen": "https://link-a-imagen-de-back-in-black.com"
+    "album": "SPILL THE FEELS",
+    "artista": "SEVENTEEN",
+    "fecha": 28/10/2024,
+    "imagen": "https://www.google.com/search?cs=1&rlz=1C1CHBF_esCL1074CL1074&sca_esv=bfb74702882ef790&q=seventeen+spill+the+feels&udm=2&fbs=AEQNm0CbCVgAZ5mWEJDg6aoPVcBgy3z6G8XHXm5Ah4uwilWjRJeyTIczQNFLgz4BekoSgG70mFPAcMtEFe4JLi1nQt_XmejUymD71cdvQ9jZVXr2fVtK1FtnrBHdRwPPC5ShYFKURYNiccuiaVTNh_e3LL-2SHsFlfCR3A4DKm4TVI-72ag_1MrXykndYrrZLgPlOs3MMBiUM8WaWIkcrm9yM_Ngilj8sQ&sa=X&ved=2ahUKEwjjrqvwyJ6JAxU0LbkGHdhJIsMQtKgLegQIExAB&biw=1366&bih=633&dpr=1#vhid=vmuZdifHIzQi7M&vssid=mosaic"
   }
 ];
 
